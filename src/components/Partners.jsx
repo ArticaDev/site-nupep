154dfb1222dd12a348686a1c2bf0ae662c3b8648
Lucas Pinheiro Moraes <lucas3.14nheiro@gmail.com>
2022-12-18 09:59:23 -0300

    💄 added swiper autoplay file styles

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -3,22 +3,22 @@ import Slider from "./Slider";
 import { SwiperSlide } from "swiper/react";
 import isMobile from "../utils/isMobile";
 import { useState, useEffect } from "react";
-import axios from 'axios';
-const CMS_URL = import.meta.env.VITE_NUPEP_CMS_DOMAIN
+import axios from "axios";
+const CMS_URL = import.meta.env.VITE_NUPEP_CMS_DOMAIN;
 
 const Partners = () => {
-  const [partners, setPartners] = useState([])
+  const [partners, setPartners] = useState([]);
 
   const formatPartners = (raw_data) => {
-    const partners_content = raw_data.data.map((data) => data.attributes)
-    partners_content.forEach(partner => {
+    const partners_content = raw_data.data.map((data) => data.attributes);
+    partners_content.forEach((partner) => {
       if (partner.Logo) {
-        const url = partner.Logo.data.attributes.url
-        partner.Logo = url
+        const url = partner.Logo.data.attributes.url;
+        partner.Logo = url;
       }
     });
     return partners_content;
-  }
+  };
 
   const getPartners = async () => {
     const result = await axios.get(`${CMS_URL}/partners?populate=*`);
@@ -28,8 +28,8 @@ const Partners = () => {
   };
 
   useEffect(() => {
-    getPartners()
-  }, [])
+    getPartners();
+  }, []);
 
   return (
     <div className="grid grid-flow-row gap-14 px-6 lg:px-16">
@@ -39,13 +39,16 @@ const Partners = () => {
       <div className="col-span-3 grid grid-flow-col justify-around gap-11 lg:px-40">
         <Slider
           sliderPerView={isMobile() ? 1 : 3}
-          autoplay
           withNavigation={false}
           withPagination={false}
+          autoplay
         >
           {partners.map((partner, index) => (
             <SwiperSlide key={index}>
-              <a className="grid cursor-pointer items-center justify-center" href={partner.Link}>
+              <a
+                className="grid cursor-pointer items-center justify-center"
+                href={partner.Link}
+              >
                 <img
                   src={partner.Logo}
                   alt="partner"
