df85191ae7007f23a4ed3831a4fdd82933c04166
LL <limalucas454@gmail.com>
2022-12-23 10:42:28 -0300

    minor fix to css

--- src/components/SwiperImageWithTitle.jsx
+++ src/components/SwiperImageWithTitle.jsx
@@ -1,10 +1,7 @@
 const SwiperImageWithTitle = ({ src, title }) => {
   return (
     <div>
-      <img
-        src={src}
-        className="h-96 w-full object-cover object-top lg:object-contain "
-      />
+      <img src={src} className="h-96 w-full object-contain" />
       <div className="grid h-32 items-center bg-black bg-opacity-90">
         <h2 className="px-6 text-xl text-white lg:px-16 lg:text-2xl">
           {title}
