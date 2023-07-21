e1817af0ca8b38baf8a200316a0b2b6f8a273ced
MarcosN <marcosnery.comp@gmail.com>
2023-07-21 03:13:07 -0300

    feat: setting up i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,32 +1,16 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-
-import en from './i18n/en.json';
-import ptBR from './i18n/pt-BR.json';
-
-const resources = {
-  'pt-BR': ptBR,
-  en,
-};
+import en from './i18n/en';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
-    fallbackLng: 'en',
+    resources: { en },
+    fallbackLng: 'pt-BR',
     interpolation: {
       escapeValue: false,
-    },
-    detection: {
-      order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag'],
-      lookupQuerystring: 'lng',
-      lookupCookie: 'language',
-      lookupLocalStorage: 'language',
-      lookupSessionStorage: 'language',
-      caches: ['localStorage'],
-      excludeCacheFor: ['cimode'],
     }
   });
 
